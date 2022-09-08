68deb809bf13ae2e3d7316f022c966443f7c664d
DebarunMitra <debarun96mitra@gmail.com>
2022-09-08 17:39:41 +0530

    removing a node from a tree added

--- src/Tree/BinaryTreeProgram.java
+++ src/Tree/BinaryTreeProgram.java
@@ -32,6 +32,53 @@ public class BinaryTreeProgram {
         }
     }
 
+    public void remove(int value) {
+        if(root==null){
+            return;
+        }
+
+        BinaryTreeNode removingNode = root,
+                        parentNode = null;
+
+        //Find the parent node
+        while (removingNode.getValue() != value){
+            parentNode = removingNode;
+            if(removingNode.getValue() > value){
+                removingNode = removingNode.getLeft();
+            }else if(removingNode.getValue() < value){
+                removingNode = removingNode.getRight();
+            }
+        }
+
+        BinaryTreeNode replacementNode = null;
+        if(removingNode.getRight() != null){
+            replacementNode = removingNode.getRight();
+            if(replacementNode.getLeft() == null){
+                replacementNode.setLeft(removingNode.getLeft());
+            }else{
+                BinaryTreeNode replacementParentNode = removingNode;
+                while(replacementNode.getLeft() != null){
+                    replacementParentNode = replacementNode;
+                    replacementNode = removingNode.getLeft();
+                }
+
+                replacementParentNode.setLeft(null);
+                replacementNode.setLeft(removingNode.getLeft());
+                replacementNode.setRight(removingNode.getRight());
+            }
+        } else if (removingNode.getLeft() != null) {
+            replacementNode = removingNode.getLeft();
+        }
+
+        if(parentNode == null) {
+            root = replacementNode;
+        } else if(parentNode.getLeft() == removingNode) { //We are a left child
+            parentNode.setLeft(replacementNode);
+        } else { //We are a right child
+            parentNode.setRight(replacementNode);
+        }
+    }
+
     int count = 0;
     public void printTree() {
         count = 0;
@@ -62,6 +109,8 @@ public class BinaryTreeProgram {
         tree.insert(30);
         tree.insert(23);
 
+        tree.printTree();
+        tree.remove(15);
         tree.printTree();
     }
 }
